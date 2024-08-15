487252ae31a74b5f286fbea20a5b4bcf3b7637b2
camilitwo <camilo.igv@gmail.com>
2024-08-15 15:04:24 -0400

    Add routing and remove placeholder HTML
    Introduce basic routing with a WelcomeComponent. Also, clean up the app.component.html by removing the placeholder content.

--- barberHub/src/app/navbar/navbar.component.ts
+++ barberHub/src/app/navbar/navbar.component.ts
@@ -1,17 +1,41 @@
 import {Component, ElementRef, HostListener, Inject, OnInit, PLATFORM_ID, Renderer2} from '@angular/core';
 import {isPlatformBrowser} from "@angular/common";
-import {RouterLink} from "@angular/router";
+import {RouterLink, RouterLinkActive} from "@angular/router";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent implements OnInit {
+
+  toggleMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    const hamburger = document.querySelector('.hamburger');
+    if (navMenu) {
+      navMenu.classList.toggle('active');
+    }
+    if (hamburger) {
+      hamburger.classList.toggle('active');
+    }
+  }
+
+// Puedes también ajustar la lógica para cerrar el menú al hacer clic en un enlace
+  closeMenu() {
+    const navMenu = document.querySelector('.nav-menu');
+    const hamburger = document.querySelector('.hamburger');
+    if (navMenu && navMenu.classList.contains('active')) {
+      navMenu.classList.remove('active');
+    }
+    if (hamburger && hamburger.classList.contains('active')) {
+      hamburger.classList.remove('active');
+    }
+  }
 
   isBrowser: boolean;
 
